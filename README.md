f61dc7858967f0ea3dd81e6bccc580ea95cf8b9f
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-26 20:32:20 +0200

    Allow usage of arrays next to min/max object

--- README.md
+++ README.md
@@ -5,10 +5,14 @@
 `$ npm install convert-range`
 
 ## Usage
+Examples:  
 ```js
-const convert = require('convert-range');
+const convertRange = require('convert-range');
 
-convert(5, {min: 0, max: 10}, {min: 0, max: 1}); // 0.5
+convertRange(3000, [0, 5000], [2500, 10000]); // 7000
+convertRange(0.6665, {min: 0, max: 1}, {min: 1000, max: 5000}); // 3666
+
+convertRange(3, [1, 10], [0, window.innerWidth]);
 ```
 
 ## License
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 module.exports = convertRange;
 
-
 /**
   * Convert value from one range to another
   * @param {Number} value value to convert
-  * @param {Object} oldRange min, max of values range
-  * @param {Object} newRange min, max of desired range
+  * @param {Object|Array} oldRange min, max of original range
+  * @param {Object|Array} newRange min, max of desired range
   * @return {Number} value converted to other range
   */
 function convertRange(value, oldRange, newRange) {
+  if (Array.isArray(oldRange) && Array.isArray(newRange)) {
+    return ((value - oldRange[0]) * (newRange[1] - newRange[0])) / (oldRange[1] - oldRange[0]) + newRange[0];
+  }
   return ((value - oldRange.min) * (newRange.max - newRange.min)) / (oldRange.max - oldRange.min) + newRange.min;
 }
